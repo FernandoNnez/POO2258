78edd014cb8fdf15697bc1ae30268c1ce14ea560
Fernando Núñez <126068178+FernandoNnez@users.noreply.github.com>
2023-05-05 10:11:22 -0600

    Excepciones en Java
    Se utilizan las palabras reservadas try, catch y finally para evitar problemas de ejecución en tiempo de cliente o ejecución.

---
+++ Exepciones/Main.java
@@ -0,0 +1,20 @@
+package Exepciones;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        try {
+            int a=10;
+            int b=Integer.parseInt(scanner.nextLine());
+            System.out.println("La div es: "+a/b);
+        } catch (ArithmeticException e){
+            //e.printStackTrace();
+            System.out.println("No puedes divivir algo entre 0!");
+        } catch (NumberFormatException nfe){
+            System.out.println("Las letras no son números...");
+        } finally {
+            System.out.println("Finally siempre se ejecuta.");
+        }
+        System.out.println("Fin.");
+    }
+}
