e2806e83162b418c2f68b5862bc01f1317790477
Fernando Núñez <126068178+FernandoNnez@users.noreply.github.com>
2023-05-24 20:01:44 -0600

    Delete Main.java

--- SQLiteCONNECT/Main.java
+++
@@ -1,9 +0,0 @@
-package SQLiteCONNECT;
-import SQLiteCONNECT.Controller.BookController;
-import SQLiteCONNECT.View.BookView;
-public class Main {
-    public static void main(String[] args) {
-        BookView viewBook=new BookView("MVC & JDBC");
-        BookController controller=new BookController(viewBook);
-    }
-}
